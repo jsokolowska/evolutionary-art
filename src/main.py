4ba52ec1fa56a4aed4df428c534ea4ec0b7c0f2b
michal-swiatek <swiatek.michal006@gmail.com>
2021-06-12 18:47:34 +0200

    Refactored fitness function, now 5x faster

--- src/main.py
+++ src/main.py
@@ -12,32 +12,40 @@ import visualization
 
 if __name__ == "__main__":
 
-    width, height = 150, 150
+    width, height = 400, 400
     iterations = 150
-    step = 10
+    step = 30
     generation = 0
     show_images = False
-    p = 10
+    p = 1
 
     fitness1, index1 = random_aesthetic()
     fitness2, index2 = random_aesthetic()
     fitness3, index3 = random_aesthetic()
     print("Aesthetic functions chosen: c1-{}, c2-{}, c3-{}".format(index1, index2, index3))
 
-    aesthetic_fitness_list = [AestheticFitness(fitness1, p, "c1"),
-                              AestheticFitness(fitness2, p, "c2"),
-                              AestheticFitness(fitness3, p, "c3"),
+    aesthetic_fitness_list = [AestheticFitness(fitness1, width, height, p, "c1"),
+                              AestheticFitness(fitness2, width, height, p, "c2"),
+                              AestheticFitness(fitness3, width, height, p, "c3"),
                               ImitationAesthetic("../target/starry_night.jpg")]
 
-    weights = [1/6, 1/6, 1/6, 1]
+    # weights = [1/6, 1/6, 1/6, 1]
+    weights = [0.6, 0.6, 0.6, 1]
+    # weights = [0, 0, 0, 1]
+    imitation = np.asarray(Image.open("../target/scream.jpg").convert("RGB")).reshape((width * height, 3))
 
     # fitness = SimpleFitness((255, 0, 255))
+    fitness = ImageFitnessFunction(width, height, fitness1, fitness2, fitness3, p, imitation.data, weights)
+    fitness_target = visualization.image_from_population(width, height, fitness.texture)
+    fitness_target.save("../results/fitness_target.png")
+
     # fitness = CompoundFitnessFunction(aesthetic_fitness_list, weights)
-    fitness = ImitationAesthetic("../target/drawisland.png")
-    initial_population = population.generate_initial_population(width, height, [255, 255, 255, 0])
+    # fitness = CompoundFitnessFunction([SimpleFitness((255, 0, 0)), SimpleFitness((0, 0, 255))])
+    # fitness = ImitationAesthetic("../target/drawisland.png")
+    initial_population = population.generate_initial_population(width, height, [255, 255, 255])
     print("Generated new population of {} individuals".format(width * height))
 
-    alg = algorithm.PSO(initial_population, 2, 2, 0.4, 0.9, iterations, width, height)
+    alg = algorithm.PSO(initial_population, 2, 2, 0.9, 0.9, iterations, width, height)
 
     for i in trange(0, iterations, step):
         img = visualization.image_from_population(width, height, alg.population)
